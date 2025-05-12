dbe81d1972002a4a4af9d93ee32e0586362f619c
Hipedisa3 <ahossam127@outlook.com>
2025-05-12 10:23:58 +0300

    Creating a calculator that writes kilobytes as bytes and bits

--- app.cpp
+++ app.cpp
@@ -1,39 +1,17 @@
 #include <iostream>
-using std::cout;
-
-/*
-===============================================
-  Backslash allows to type special characters
-that cannot be typed under normal circumstances
-===============================================
--- \n New line
--- \\ Prints backslash
--- \' Prints single quote
--- \" Prints double quote
--- \t Prints a tab
--- \r Makes cursor at start of sentence and types the rest of sentence, overwriting the characters from the start
-*/
-
-// Floats are less precise (7 digits) but only 4 bytes
-// Doubles are more precise (15 digits) but are 8 bytes
-
-double y = 3.3;
 
 int main()
 {
-  // cout << "Line 1\n";
-  // cout << "Line 2\\\n";
-  // cout << "Line 3 \"4\"\n";
-  // cout << "Line 5\t\t6\n";
-  // cout << "Ahmed\rHossam";
+  int kilobytes;
 
-  // New Line
-  // New Line2
+  std::cout << "Enter number of kilobytes: ";
+  std::cin >> kilobytes;
 
-  int x = 12;
-  x = 10;
+  int bytes = kilobytes * 1024;
+  int bits = bytes * 8;
 
-  cout << x;
-
+  std::cout << "Number of kilobytes: " << kilobytes << " kilobytes\n";
+  std::cout << "Number of bytes: " << bytes << " bytes\n";
+  std::cout << "Number of bits: " << bits << " bits";
   return 0;
 }
