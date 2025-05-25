2f167ce0913ca3e4773c3112add67499049db1f5
Hipedisa3 <ahossam127@outlook.com>
2025-05-25 11:39:38 +0300

    Simple even / odd checker

--- app.cpp
+++ app.cpp
@@ -1,39 +1,19 @@
 #include <iostream>
+using std::cin;
 using std::cout;
-
-/*
-===============================================
-  Backslash allows to type special characters
-that cannot be typed under normal circumstances
-===============================================
--- \n New line
--- \\ Prints backslash
--- \' Prints single quote
--- \" Prints double quote
--- \t Prints a tab
--- \r Makes cursor at start of sentence and types the rest of sentence, overwriting the characters from the start
-*/
-
-// Floats are less precise (7 digits) but only 4 bytes
-// Doubles are more precise (15 digits) but are 8 bytes
-
-double y = 3.3;
 
 int main()
 {
-  // cout << "Line 1\n";
-  // cout << "Line 2\\\n";
-  // cout << "Line 3 \"4\"\n";
-  // cout << "Line 5\t\t6\n";
-  // cout << "Ahmed\rHossam";
+    int num;
 
-  // New Line
-  // New Line2
+    cout << "====== Even / Odd Checker ======\n";
+    cout << "Enter a number: ";
+    cin >> num;
 
-  int x = 12;
-  x = 10;
+    if (num % 2 == 0)
+        cout << "Number is even";
+    else
+        cout << "Number is odd";
 
-  cout << x;
-
-  return 0;
+    return 0;
 }
