9232cb0d76044e24706a758ea3524bc75761c636
Hipedisa3 <ahossam127@outlook.com>
2025-06-05 01:46:15 +0300

    programmed app.cpp

--- app.cpp
+++ app.cpp
@@ -1,88 +1,58 @@
+/*
+  Control Flow
+  - Switch
+*/
+
 #include <iostream>
-using std::cin;
-using std::cout;
+using namespace std;
 
 int main()
 {
-    // App 1 - Even / Odd Checker
-    // int num;
 
-    // cout << "====== Even / Odd Checker ======\n";
-    // cout << "Enter a number: ";
-    // cin << num;
-
-    // if (num % 2 == 0)
-    //     cout << "Number is even";
-    // else
-    //     cout << "Number is odd";
-
-    // App 2 - Find Greatest Number
-
-    // int a, b, c;
-
-    // cout << "====== Greatest Number Finder ======\n";
-    // cout << "Enter three numbers: \n";
-    // cin << a << b << c;
-
-    // if (a > b && a > c)
-    //     cout << a << " is the greatest number";
-    // else if (b > a && b > c)
-    //     cout << b << " is the greatest number";
-    // else
-    //     cout << c << " is the greatest number";
-
-    // App 3 - User Rank Checker
-
-    // std::string rank;
-    // int points;
-
-    // cout << "====== User Rank Checker ======\n";
-    // cout << "Enter your points: ";
-
-    // cin >> points;
-
-    // if (points < 500)
+    // if (day == 1)
     // {
-    //     rank = "Cat";
-    //     cout << "Your rank is " << rank;
+    //     cout << "Open From 08:00 To 14:00";
     // }
-    // else if (points >= 500 && points < 1000)
+    // else if (day == 2)
     // {
-    //     rank = "Cheetah";
-    //     cout << "Your rank is " << rank;
+    //     cout << "Open From 08:00 To 14:00";
     // }
-    // else if (points >= 100 && points < 10000)
+    // else if (day == 3)
     // {
-    //     rank = "Lion";
-    //     cout << "Your rank is " << rank;
+    //     cout << "Open From 10:00 To 16:00";
     // }
     // else
     // {
-    //     rank = "Dragon";
-    //     cout << "Your rank is " << rank;
+    //     cout << "Closed";
     // }
 
-    // App 4 - User Rank Checker
-    // int a, b, op;
+    int points;
+    cout << "Enter your points: ";
+    cin >> points;
 
-    // cout << "====== Simple Calculator ======\n";
-    // cout << "Choose two numbers: \n";
-    // cin >> a >> b;
-    // cout << "\nPick an operator: [1, 2, 3, 4]\n";
-    // cout << "[1] +\n[2] -\n[3] *\n[4] /\n";
-    // cin >> op;
+    switch (points)
+    {
+    case 100:
+    case 110:
+    case 120:
+        cout << "You won an iPhone!\n";
+        points = 0;
+        break;
+    case 400:
+        cout << "You won an iPad\n";
+        points = 0;
+        break;
+    case 500:
+        cout << "You won a PS5\n";
+        points = 0;
+        break;
+    default:
+        cout << "You won nothing\n";
+        points = 0;
+        break;
+    }
 
-    // if (op == 1)
-    //     cout << a << " + " << b << " is " << a + b << std::endl;
-    // else if (op == 2)
-    //     cout << a << " - " << b << " is " << a - b << std::endl;
-    // else if (op == 3)
-    //     cout << a << " * " << b << " is " << a * b << std::endl;
-    // else if (op == 4)
-    //     cout << a << " / " << b << " is " << a / b << std::endl;
-    // else
-    //     cout << "Please enter a valid operator\n";
+    system("pause");
 
-    // system("pause");
     return 0;
 }
